c03f2c519781573f190eab5f0f5ba475b9c8caac
eric lee <ericleejh@gmail.com>
2019-11-06 20:50:59 +0800

    updated some of the changes

---
+++ src/Controller/DisplayController.java
@@ -0,0 +1,25 @@
+package Controller;
+
+public class DisplayController implements ControllerInterface {
+    BookingController bookingController;
+    CineplexController cineplexController;
+    MovieController movieController;
+    PricingChartController pricingChartController;
+    PublicHolidayController publicHolidayController;
+    ShowTimeController showTimeController;
+    TransactionController transactionController;
+
+    public DisplayController(){
+        //cineplexController = new CineplexController();
+    }
+
+
+    public void viewAllCineplex(){
+        //cineplexController.
+    }
+
+    @Override
+    public void updateDat() {
+
+    }
+}
